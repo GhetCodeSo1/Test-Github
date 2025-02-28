5568fe34940f595231912e741589a94006c4329f
GhetCodeSo1 <23520376@gm.uit.edu.vn>
2025-02-28 08:44:02 +0700

    test 6

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
-  res.send("Thang Duy dep trai vai ccac");
+  res.send("Thang Duy dep trai vai ccac 12312571258158");
 });
 
 app.listen(port, () => {
